48842827b29ef5179542fe190b732157011aab6a
Eleven <qiuqiujun666@gmail.com>
2022-01-25 17:53:33 -0600

    refactor: 优化 try 块和日志
    Signed-off-by: Eleven <qiuqiujun666@gmail.com>

--- update_database.py
+++ update_database.py
@@ -1,15 +1,10 @@
 import asyncio
 import json
 import logging.config
+import logging.handlers
 
 import pymysql
 import websockets
-
-with open("config/logging.json", "r") as logging_config_file:
-    logging_config = json.load(logging_config_file)
-
-logging.config.dictConfig(logging_config)
-develop_logger = logging.getLogger("develop")
 
 
 class Mysql(object):
@@ -20,15 +15,15 @@ class Mysql(object):
     def __init__(self):
         with open("config/database.json") as config:
             database = json.load(config)
-            self.conn = pymysql.connect(host=database["host"], port=database["port"], db=database["database"],
-                                        user=database["user"], password=database["password"])
 
-    async def update_death_event(self, payload):
+        self.conn = pymysql.connect(host=database["host"], port=database["port"], db=database["database"],
+                                    user=database["user"], password=database["password"])
+
+    async def update_death_event_in_background(self, payload):
         """击杀数据库更新
 
         :param payload: Websocket 订阅数据，字典类。
         """
-        # 数据库更新
         with self.conn.cursor() as cursor:
             sql = "INSERT INTO ps2_death (attacker_character_id, attacker_fire_mode_id, attacker_loadout_id, " \
                   "attacker_vehicle_id, attacker_weapon_id, character_id, character_loadout_id, is_headshot, " \
@@ -40,52 +35,83 @@ class Mysql(object):
                                  payload["world_id"], payload["zone_id"]))
             self.conn.commit()
 
-    async def update_alert_event(self, payload):
+        develop_logger.debug(f"UPDATE {payload}")
+
+    async def update_alert_event_in_background(self, payload):
         """ 警报数据库更新
 
         :param payload: Websocket 订阅数据，字典类。
         """
-        # 数据库更新
-        print(payload)
+        develop_logger.debug(f"UPDATE {payload}")
 
 
-async def connect_websocket():
-    # Websocket API 订阅内容，http://census.daybreakgames.com/
-    ps_api = "wss://push.planetside2.com/streaming?environment=ps2&service-id=s:yinxue"
-    subscribe = '{"service":"event","action":"subscribe","characters":["all"],"eventNames":["Death", ' \
-                '"MetagameEvent"],"worlds":["1", "10", "13", "17", "40"],"logicalAndCharactersWithWorlds":true} '
-    # 连接数据库
-    mysql = Mysql()
+class SynchronizeSubscribe(object):
+    """ 同步订阅事件至数据库，使用 Websockets
 
-    async with websockets.connect(ps_api, ping_timeout=None) as websocket:
-        await websocket.send(subscribe)
-        print("Pending for message...")
+    """
 
-        while True:
-            message = await websocket.recv()
-            data: dict = json.loads(message)
+    def __init__(self):
+        # Websocket API 订阅内容，http://census.daybreakgames.com/
+        self.ps_api = "wss://push.planetside2.com/streaming?environment=ps2&service-id=s:yinxue"
+        self.subscribe = '{"service":"event","action":"subscribe","characters":["all"],"eventNames":["Death", ' \
+                         '"MetagameEvent"],"worlds":["1", "10", "13", "17", "40"],' \
+                         '"logicalAndCharactersWithWorlds":true} '
+        self.mysql = Mysql()
 
-            # 是否为订阅事件
-            if not (True and data.get("service") == "event" and data.get("type") == "serviceMessage"):
-                continue
+    async def connect_ps_api(self):
+        """ 连接行星边际 API 接口
 
-            # 判断事件选择数据库操作
-            payload: dict = data["payload"]
+        """
+        async with websockets.connect(self.ps_api, ping_timeout=None) as ws:
+            production_logger.info("Connection established.")
+            await ws.send(self.subscribe)
+            while True:
+                message = await ws.recv()
+                data: dict = json.loads(message)
 
-            if payload.get("event_name") == "Death":
-                await mysql.update_death_event(payload)
+                # 是否为订阅事件
+                if not await self.is_subscribe_event(data):
+                    continue
 
-            elif payload.get("event_name") == "MetagameEvent":
-                await mysql.update_alert_event(payload)
+                await self.match_event_name(data)
+
+    @staticmethod
+    async def is_subscribe_event(data):
+        return True and data.get("service") == "event" and data.get("type") == "serviceMessage"
+
+    async def match_event_name(self, data):
+        """ 匹配事件对应的数据库操作
+
+        :param data: API 返回数据，字典
+        """
+        payload: dict = data["payload"]
+
+        if payload.get("event_name") == "Death":
+            await self.mysql.update_death_event_in_background(payload)
+
+        elif payload.get("event_name") == "MetagameEvent":
+            await self.mysql.update_alert_event_in_background(payload)
 
 
 if __name__ == '__main__':
+    # 从文件中读取日志配置
+    with open("config/logging.json", "r") as logging_config_file:
+        logging_config = json.load(logging_config_file)
+
+    logging.config.dictConfig(logging_config)
+    develop_logger = logging.getLogger("develop")
+    production_logger = logging.getLogger("production")
+
+    synchronize = SynchronizeSubscribe()
+
     while True:
         try:
-            asyncio.run(connect_websocket())
+            asyncio.run(synchronize.connect_ps_api())
 
         except KeyboardInterrupt:
+            production_logger.info("The program was closed by the user.")
             break
 
         except websockets.WebSocketException:
+            production_logger.warning("Connection failed, try to reconnect.")
             continue

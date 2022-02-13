1f0cfbb8cefdbe8922cf4d2fef8648e14ad165fd
Eleven <53446819+voidswordQQJ@users.noreply.github.com>
2022-02-13 04:10:43 -0600

    feat: setup.py 安装程序

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PS2-DatabaseSync',
+    version='3.0.0',
+    packages=['tests', 'hydrogen'],
+    url='https://github.com/PlanetSide2-CPC/PS2-DatabaseSync',
+    license='GNU General Public License v3.0',
+    author='Ecss11',
+    author_email='2404919628@qq.com',
+    description='行星边际2 同步 API 订阅信息至数据库'
+)
